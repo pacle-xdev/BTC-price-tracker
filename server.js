fe163d1d864b697ea58b862dc01f95946db75a27
pacle002 <pa.techdev007@gmail.com>
2023-05-21 23:18:53 -0300

    updated the last building erros

---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const path = require("path");
+const os = require("os");
+const express = require("express");
+const socketio = require("socket.io");
+const fetchbtc = require("./fetchbtc");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+const PORT = process.env.PORT || 1978;
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  console.log("Client connected");
+  try {
+    setInterval(async () => {
+      socket.emit(
+        "subscribed-btc-prices",
+        await fetchbtc.pushUpdates().catch((err) => {
+          console.log(err);
+        })
+      );
+    }, 5000);
+  } catch (err) {}
+});
+
+server.listen(PORT, () =>
+  console.log(`BTCTicker server running on port ${PORT}`)
+);
